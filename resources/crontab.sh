0814f2e3c6b641a8a8025ae21bac6c81765a08b9
Teo <onlyteo@live.com>
2017-08-19 21:31:05 +0200

    Append to log

--- resources/crontab.sh
+++ resources/crontab.sh
@@ -2,4 +2,4 @@
 # Crontab for Let's Encrypt
 #
 
-10 5 * * * "${LETSENCRYPT_HOME}/bin/letsencrypt-wrapper.sh" > "${LETSENCRYPT_LOG_DIR}/letsencrypt.log"
+10 5 * * * "${LETSENCRYPT_HOME}/bin/letsencrypt-wrapper.sh" >> "${LETSENCRYPT_LOG_DIR}/letsencrypt.log"
